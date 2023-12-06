586dd09b5d2260213a89f689d84f2ef5eb22ed53
MAmjadd7 <MAmjadd7@hotmail.com>
2023-12-06 14:30:26 +0500

    I have complete also the backend of calculator App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,11 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+//    expression dependencies
+//    implementation("net.objecthunter:exp4j:0.4.5")
+//    implementation("org.mariuszgromada.math:MathParser.org-mXparser:4.4.2")
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/imjad/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/imjad/calculatorapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.imjad.calculatorapp
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.imjad.calculatorapp.databinding.ActivityMainBinding
+import java.util.Stack
 import kotlin.properties.Delegates
 
 
@@ -16,7 +19,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
 
         binding.btnZero.setOnClickListener(this)
         binding.btnOne.setOnClickListener(this)
@@ -35,6 +37,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.btnMinus.setOnClickListener(this)
         binding.btnEqual.setOnClickListener(this)
         binding.btnClear.setOnClickListener(this)
+        binding.btnClearAll.setOnClickListener(this)
 
 
     }
@@ -46,7 +49,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             binding.btnZero.id -> {
                 if (values == "0") {
                     binding.textViewValue.text = "0"
-                } else {
+                }
+                else if (values.endsWith("/0")) {
+                    return
+                }
+                else {
                     binding.textViewValue.text = values + "0"
                 }
             }
@@ -86,8 +93,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             binding.btnFive.id -> {
                 if (values == "0") {
                     binding.textViewValue.text = "5"
-                }
-                else {
+                } else {
                     binding.textViewValue.text = values + "5"
                 }
             }
@@ -127,108 +133,162 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             binding.btnDot.id -> {
                 if (values == "0") {
                     binding.textViewValue.text = "0."
-                }
-                else {
-                    if (values == "0." || values.contains(".")) {
+                } else {
+                    if (values == "0." || values.last() == '.' || values.last() == '*' || values.last() == '/' || values.last() == '+' || values.last() == '-') {
                         return
-                    }
-                    else if (values == "-") {
-                        binding.textViewValue.text = values+"0."
-                    }
-                    else {
+                    } else if (values == "-") {
+                        binding.textViewValue.text = values + "0."
+                    } else {
                         binding.textViewValue.text = "$values."
                     }
-
                 }
             }
 
-            binding.btnClear.id -> {
+            binding.btnClearAll.id -> {
                 binding.textViewValue.text = "0"
                 binding.textViewResult.text = ""
             }
 
+            binding.btnClear.id -> {
+                binding.textViewValue.text = values.dropLast(1)
+            }
+
             binding.btnDivide.id -> {
-                if (values == "-"){
+                if (values == "-") {
                     binding.textViewValue.text = "0"
-                }
-                else if (values.last() == '/'  || values.last() == '+' || values.last() == '-' || values.last() == 'x') {
+                } else if (values.last() == '/' || values.last() == '+' || values.last() == '-' || values.last() == '*') {
 //                    val removedLast = binding.textViewValue.text.toString().dropLast(1)
                     binding.textViewValue.text = values.dropLast(1) + "/"
 
-                }
-                else if( values == "0"|| values.last() == '.'){
+                } else if (values == "0" || values.last() == '.') {
                     return
-                }
-
-                else {
+                } else {
                     binding.textViewValue.text = "$values/"
                 }
             }
 
             binding.btnMultiply.id -> {
-                if (values == "-"){
+                if (values == "-") {
                     binding.textViewValue.text = "0"
-                }
-                else if (values.last() == '/'  || values.last() == '+' || values.last() == '-' || values.last() == 'x') {
+                } else if (values.last() == '/' || values.last() == '+' || values.last() == '-' || values.last() == '*') {
 //                    val removedLast = binding.textViewValue.text.toString().dropLast(1)
-                    binding.textViewValue.text = values.dropLast(1) + "x"
+                    binding.textViewValue.text = values.dropLast(1) + "*"
 
-                }
-                else if( values == "0"|| values.last() == '.'){
+                } else if (values == "0" || values.last() == '.') {
                     return
-                }
-                else {
-                    binding.textViewValue.text = values + "x"
+                } else {
+                    binding.textViewValue.text = "$values*"
                 }
             }
 
             binding.btnPlus.id -> {
-                if (values == "-"){
+                if (values == "-") {
                     binding.textViewValue.text = "0"
-                }
-                else if (values.last() == '/'  || values.last() == '+' || values.last() == '-' || values.last() == 'x') {
+                } else if (values.last() == '/' || values.last() == '+' || values.last() == '-' || values.last() == '*') {
 
                     binding.textViewValue.text = values.dropLast(1) + "+"
 
-                }
-                else if( values == "0"|| values.last() == '.'){
+                } else if (values == "0" || values.last() == '.') {
                     return
-                }
-
-                else {
-                    val value= binding.textViewValue.text.toString()
+                } else {
+                    val value = binding.textViewValue.text.toString()
 
                     binding.textViewValue.text = "$value+"
                 }
             }
 
             binding.btnMinus.id -> {
-                if (values.last() == '/'  || values.last() == '+' || values.last() == '-' || values.last() == 'x') {
+                if (values.last() == '/' || values.last() == '+' || values.last() == '-' || values.last() == '*') {
 
                     binding.textViewValue.text = values.dropLast(1) + "-"
 
-                }
-                else if(values.last() == '.'){
+                } else if (values.last() == '.') {
                     return
-                }
-                else if(values == "0"){
+                } else if (values == "0") {
                     binding.textViewValue.text = "-"
-                }
-                else {
+                } else {
                     binding.textViewValue.text = "$values-"
                 }
             }
 
             binding.btnEqual.id -> {
+//                val getResult = binding.textViewValue.text.toString()
+//                if (getResult.last() == '/' || getResult.last() == '*' || getResult.last() == '+' || getResult.last() == '-') {
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        "Please remove the last sign",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                } else if (getResult.endsWith("/0")) {
+//                    Toast.makeText(this@MainActivity, "Can't divide by 0", Toast.LENGTH_SHORT)
+//                        .show()
+//                } else {
+//                    val expression = ExpressionBuilder(getResult).build()
+//                    val result = expression.evaluate()
+//                    val longResult = result.toLong()
+//                    if (result == longResult.toDouble()) {
+//                        binding.textViewResult.text = longResult.toString()
+//                    } else {
+//                        binding.textViewResult.text = result.toString()
+//                    }
+//                }
 
+                sol(binding.textViewValue)
             }
-
         }
     }
-    private fun getInputExpression(): String {
-        var expression = binding.textViewValue.text.replace(Regex("÷"), "/")
-        expression = expression.replace(Regex("×"), "*")
-        return expression
+
+
+    fun applyOp(operatorX: Char, a: Float, b: Float): Float {
+        when (operatorX) {
+            '+' -> return b + a
+            '-' -> return b - a
+            '*' -> return b * a
+            '/' -> {
+                if (a != 0F) return b / a
+            }
+        }
+        return 0F
+    }
+    fun checkP(operatorA: Char, operatorB: Char): Boolean {
+        return !(( operatorA == '+' || operatorA == '-') && ( operatorB == '*' || operatorB == '/'))
     }
 
+    fun letsCalculate(mS: String): Float {
+        val myChar = mS.toCharArray()
+        val myNumStack = Stack<Float>()
+        val operatorStack = Stack<Char>()
+        var i = 0
+        while (i < myChar.size) {
+            if ((myChar[i] in '0'..'9') || myChar[i] == '.') {
+                val stringBuffer = StringBuilder()
+                while (i < myChar.size && (myChar[i] in '0'..'9' || myChar[i] == '.')) {
+                    stringBuffer.append(myChar[i++])
+                }
+                myNumStack.push(stringBuffer.toString().toFloat())
+                i--
+            }  else if (myChar[i] == '/' || myChar[i] == '*' || myChar[i] == '+' || myChar[i] == '-') {
+                while (!operatorStack.empty() && checkP(myChar[i], operatorStack.peek())) {
+                    myNumStack.push( applyOp(operatorStack.pop(),myNumStack.pop(),myNumStack.pop()))
+                }
+                operatorStack.push(myChar[i])
+            }
+            i++
+        }
+        while (!operatorStack.empty()) {
+            myNumStack.push(applyOp(operatorStack.pop(), myNumStack.pop(), myNumStack.pop()))
+        }
+        return myNumStack.pop()
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun sol(x: View?) {
+        try {
+            binding.textViewResult.setTextColor(Color.BLACK)
+            binding.textViewResult.text = "${letsCalculate(binding.textViewValue.text.toString())}"
+        } catch (e: Exception) {
+            binding.textViewResult.setTextColor(Color.RED)
+            binding.textViewResult.text = "Error"
+        }
+    }
 }
